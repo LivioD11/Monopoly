0a92fe9a887440e2f0c8ee544a7803c29cdaf379
Lorenzo <lorenzo.distefano@samtrevano.ch>
2026-02-28 22:11:03 +0100

    Nuova struttura progetto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player("Cacca", 'c');
+        Player p1 = new Player("Prova", 'p');
         p1.print();
         Bank.print();
     }
