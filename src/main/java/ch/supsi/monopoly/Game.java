9f8030110d2432d47729b4277283c1006a3c98b6
Lorenzo <lorenzo.distefano@supsi.ch>
2026-05-02 18:53:23 +0200

    Corregione menu Game

--- src/main/java/ch/supsi/monopoly/Game.java
+++ src/main/java/ch/supsi/monopoly/Game.java
@@ -6,58 +6,64 @@ import ch.supsi.monopoly.board.BoxStart;
 import ch.supsi.monopoly.board.jail.BoxJail;
 import ch.supsi.monopoly.cli.Color;
 import ch.supsi.monopoly.cli.TextFormatter;
-import ch.supsi.monopoly.utilities.MenuOption;
-import ch.supsi.monopoly.utilities.PlayerFactory;
-import ch.supsi.monopoly.utilities.ScannerUtilities;
+import ch.supsi.monopoly.utilities.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     private final Board board;
     private final Player[] players;
-    private static final int PLAYERS_NUMBER = Config.getInt("game.players.number", 0);;
+    private static final int PLAYERS_NUMBER = Config.getInt("game.players.number", 0);
+    private Menu menu;
+    private int turnIndex;
+    private boolean isGameOver;
 
     public Game() {
         PlayerFactory setupManager = new PlayerFactory(PLAYERS_NUMBER);
+        this.menu = new Menu("");
+        this.setupMenu();
+        this.isGameOver = false;
+        this.turnIndex = 0;
         this.players = setupManager.setup();
         this.board = new Board(players);
         play();
     }
 
+    private void setupMenu(){
+        Option option1 = new Option(
+                "Visualizza saldo",
+                () -> {
+                    Player player = players[turnIndex % PLAYERS_NUMBER];
+                    System.out.println(player.toString() + TextFormatter.color(TextFormatter.formatCurrency(player.getBalance()), Color.YELLOW));
+                }
+        );
+
+        Option option2 = new Option(
+                "Tira dadi",
+                () -> {
+                    this.executeTurn();
+                }
+        );
+
+        menu.addOption(option1);
+        menu.addOption(option2);
+    }
+
     private void play() {
-        int turnIndex = 0;
-        boolean isGameOver = false;
-
         board.draw();
-
         while (!isGameOver) {
             Player currentPlayer = players[turnIndex % PLAYERS_NUMBER];
-            showMenu(currentPlayer);
-
-            int choice = ScannerUtilities.getInputInt("Scegli un'opzione: ");
-            MenuOption option = MenuOption.fromInt(choice);
-
-            switch (option) {
-                case VIEW_BALANCE ->
-                        System.out.println("Saldo di " + currentPlayer.getName() + ": " + TextFormatter.formatCurrency(currentPlayer.getBalance()));
-
-                case ROLL_DICE -> {
-                    executeTurn(currentPlayer);
-                    if (currentPlayer.isBroke()) {
-                        System.out.println(currentPlayer.getName() + "è andato in bancarotta!");
-                        isGameOver = true;
-                    } else {
-                        board.draw();
-                        turnIndex++; // Prossimo turno solo se il giocatore ha mosso
-                    }
-                }
-
-                default -> System.out.println(TextFormatter.color("Opzione non valida! Riprova.", Color.YELLOW));
-            }
+            menu.setDescription("Turno di "+currentPlayer.getName());
+            menu.displayAndSelect();
         }
         System.out.println("\n--- GARA CONCLUSA ---");
     }
 
-    private void executeTurn(Player player) {
+    private void executeTurn() {
+        Player player = players[turnIndex % PLAYERS_NUMBER];
+
         int roll1 = Dice.roll();
         int roll2 = Dice.roll();
         int roll = roll1 + roll2;
@@ -86,18 +92,19 @@ public class Game {
         Box currentBox = board.getBox(newPos);
         System.out.println(" Atterrato su: " + currentBox.getName());
         currentBox.applyEffect(player);
+
+        if (player.isBroke()) {
+            System.out.println(player.toString() + "è andato in bancarotta!");
+            isGameOver = true;
+        } else {
+            board.draw();
+            turnIndex++; // Prossimo turno solo se il giocatore ha mosso
+        }
     }
 
     private boolean checkEqualityDice(int roll1, int roll2) {
         if (roll1 == roll2)
             return true;
         return false;
-    }
-
-    private void showMenu(Player player) {
-        System.out.println(TextFormatter.color("\n=== TURNO DI: " + player.getName().toUpperCase() + " ===",Color.CYAN));
-        for (MenuOption option : MenuOption.values()) {
-            if (option != MenuOption.UNKNOWN) System.out.println(option);
-        }
     }
 }
--- src/main/java/ch/supsi/monopoly/utilities/Menu.java
+++ src/main/java/ch/supsi/monopoly/utilities/Menu.java
@@ -86,4 +86,8 @@ public class Menu {
             }
         }
     }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
 }
